3a86876e4ddda0ec0c3dd64b3a24b88630b18469
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-06 17:15:38 +0300

    worked on adding the Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Navbar, Description } from "../components";
 export default function Home() {
   return (
     <main>
-      <Description />
+      {/* <Description /> */}
       <Navbar />
       <h2>Hello CAOCAPx</h2>
     </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,51 @@
 "use client";
 
-import { motion } from "framer-motion";
-
-import styles from "../styles";
-import { navVariants } from "../utils/motion";
+import { Disclosure } from "@headlessui/react";
+import {
+  UserCircleIcon,
+  MoonIcon,
+  BellIcon,
+  CubeIcon,
+} from "@heroicons/react/24/solid";
 
 export default function Navbar() {
   return (
-    <motion.nav
-      variants={navVariants}
-      initial="hidden"
-      whileInView="show"
-      className={`${styles.xPaddings} py-8 relative`}
-    >
-      <div className="absolute w-[50%] inset-0 gradient-01" />
-    </motion.nav>
+    <>
+      <Disclosure as="nav" className="bg-black">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <button
+              type="button"
+              className="p-1 text-yellow-500 hover:text-white "
+            >
+              <CubeIcon className="h-7 w-7" />
+            </button>
+            <div className="flex flex-1 items-center justify-center">
+              <p>Explore</p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="p-1 text-gray-400 hover:text-white "
+              >
+                <MoonIcon className="h-5 w-5" />
+              </button>
+              <button
+                type="button"
+                className="p-1 text-gray-400 hover:text-white "
+              >
+                <BellIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+              <button
+                type="button"
+                className="p-1 text-purple-600 hover:text-white "
+              >
+                <UserCircleIcon className="h-8 w-8" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+    </>
   );
 }
