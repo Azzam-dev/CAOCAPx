c16a8adaa41b08b06c371508437ebd09e69fc854
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-06 23:49:10 +0300

    worked on adding a bottom nav bar and styling it

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,68 @@
 "use client";
 
+import { useState } from "react";
+import {
+  ThemeProvider,
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import {
+  RocketLaunchIcon,
+  HomeIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/solid";
+
 export default function NavBar() {
-  return <div>Explore | Build | Colab</div>;
+  const [value, setValue] = useState(0);
+  const theme = createTheme({
+    components: {
+      MuiBottomNavigation: {
+        styleOverrides: {
+          root: {
+            position: "fixed",
+            bottom: 50,
+            width: 300,
+            backgroundColor: "black",
+            borderRadius: 10,
+          },
+        },
+      },
+      MuiBottomNavigationAction: {
+        styleOverrides: { root: { color: "white" } },
+      },
+    },
+  });
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="Explore"
+            icon={<RocketLaunchIcon className="h-6 w-6" aria-hidden="true" />}
+          />
+          <BottomNavigationAction
+            label="Build"
+            icon={<HomeIcon className="h-6 w-6" aria-hidden="true" />}
+          />
+          <BottomNavigationAction
+            label="Colab"
+            icon={<UserGroupIcon className="h-6 w-6" aria-hidden="true" />}
+          />
+        </BottomNavigation>
+      </ThemeProvider>
+    </Box>
+  );
 }
