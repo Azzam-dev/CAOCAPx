8176f4224a9fab2a27526b171211360f6f3a8d92
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-07 17:00:33 +0300

    added new signin & login pages

--- src/app/(registration)/login/page.tsx
+++ src/app/(registration)/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <>
-      <h1>Hello, this is the blog page...!</h1>
+      <h1>Hello, this is the login page...!</h1>
       <h2>Sorry, But we are still working on it</h2>
     </>
   );
