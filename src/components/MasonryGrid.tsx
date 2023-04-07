a1435b1f165dc60198a2463e9403bdff545d6f6b
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-07 08:41:40 +0300

    fixed MasonryGrid layout issue

--- src/components/MasonryGrid.tsx
+++ src/components/MasonryGrid.tsx
@@ -1,34 +1,54 @@
 export default function MasonryGrid() {
   return (
-    <div className="p-4 grid gap-4 grid-cols-2 md:grid-cols-3">
-      <div className="bg-gray-300 text-black flex items-center justify-center h-40 max-w-full rounded-lg">
+    <div className="p-4 gap-5 columns-2 md:columns-3 lg:columns-5">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-square max-w-full rounded-lg overflow-hidden">
         cube 1:1
       </div>
-      <div className="bg-gray-300 text-black flex items-center justify-center h-40 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-square max-w-full rounded-lg overflow-hidden">
         cube 1:1
       </div>
-      <div className="bg-gray-300 text-black flex items-center justify-center h-40 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-square max-w-full rounded-lg overflow-hidden">
         cube 1:1
       </div>
 
-      <div className="bg-gray-300 text-black flex items-center justify-center h-40 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[2/3] max-w-full rounded-lg overflow-hidden">
         cube 2:3
       </div>
-      <div className="bg-gray-300 text-black flex items-center justify-center h-40 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[2/3] max-w-full rounded-lg overflow-hidden">
         cube 2:3
       </div>
-      <div className="bg-gray-300 text-black flex items-center justify-center h-40 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[2/3] max-w-full rounded-lg overflow-hidden">
         cube 2:3
       </div>
 
-      <div className="bg-gray-300 text-black flex items-center justify-center h-60 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[3/2] max-w-full rounded-lg overflow-hidden">
         cube 3:2
       </div>
-      <div className="bg-gray-300 text-black flex items-center justify-center h-60 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[3/2] max-w-full rounded-lg overflow-hidden">
         cube 3:2
       </div>
-      <div className="bg-gray-300 text-black flex items-center justify-center h-60 max-w-full rounded-lg">
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[3/2] max-w-full rounded-lg overflow-hidden">
         cube 3:2
+      </div>
+
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[9/16] max-w-full rounded-lg overflow-hidden">
+        cube 9:16
+      </div>
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[9/16] max-w-full rounded-lg overflow-hidden">
+        cube 9:16
+      </div>
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[9/16] max-w-full rounded-lg overflow-hidden">
+        cube 9:16
+      </div>
+
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[16/9] max-w-full rounded-lg overflow-hidden">
+        cube 16:9
+      </div>
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[16/9] max-w-full rounded-lg overflow-hidden">
+        cube 16:9
+      </div>
+      <div className="bg-gray-300 text-black flex items-center justify-center mb-5 aspect-[16/9] max-w-full rounded-lg overflow-hidden">
+        cube 16:9
       </div>
     </div>
   );
