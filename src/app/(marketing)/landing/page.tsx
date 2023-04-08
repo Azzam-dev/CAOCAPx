13a9a5d154ccb95f47bea700f68ed66920c89190
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-09 02:29:46 +0300

    fix small issue in the landing page

--- src/app/(marketing)/landing/page.tsx
+++ src/app/(marketing)/landing/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           <p
             className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
           >
-            you startup's MVP
+            you startup&apos;s MVP
           </p>
         </Link>
 
