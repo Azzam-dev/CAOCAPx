cce66cad517269ca83cd44f6a92c6a2db56e8b99
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-22 16:31:32 +0300

    styling the commandPaletteButton

--- src/components/CommandPalette/CommandPaletteButton.tsx
+++ src/components/CommandPalette/CommandPaletteButton.tsx
@@ -4,13 +4,13 @@ export default function CommandPaletteButton() {
   return (
     <button
       type="button"
-      className="inline-flex items-center rounded-md  px-3 py-1 text-sm text-gray-100 ring-1 ring-inset ring-gray-400 hover:bg-gray-900"
+      className="inline-flex items-center rounded-lg  px-2 py-1 text-sm text-gray-100 ring-1 ring-inset ring-gray-400 hover:bg-gray-900"
     >
-      <span>Command Menu .</span>
-      <p className="rounded-lg  px-2 py-1 text-sm text-gray-400 shadow-sm ring-1 ring-inset ring-gray-400">
+      <span>Command Menu</span>
+      <p className="mx-2 my-[2px] rounded-md px-2 py-[2px] text-gray-400 shadow-sm ring-1 ring-inset ring-gray-400">
         ⌘
       </p>
-      <p className="rounded-lg px-3 py-1 text-sm text-gray-400 shadow-sm ring-1 ring-inset ring-gray-400">
+      <p className="rounded-md px-2 py-[2px] text-gray-400 shadow-sm ring-1 ring-inset ring-gray-400">
         K
       </p>
     </button>
