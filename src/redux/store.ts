0b29c9869a4385831e43b9937d0ddc502b42b460
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-23 06:42:29 +0300

    hotfix: Type error: Property 'counter' does not exist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,14 @@
 "use client";
 
 import { configureStore } from "@reduxjs/toolkit";
+import CounterReducer from "./features/counterSlice";
 import CommandPaletteReducer from "./features/CommandPaletteSlice";
 
 export const store = configureStore({
   reducer: {
+    counter: CounterReducer,
     commandPalette: CommandPaletteReducer,
+
     // posts: postsReducer,
     // comments: commentsReducer,
     // users: usersReducer,
