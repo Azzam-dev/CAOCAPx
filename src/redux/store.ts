40c9b3eeb7032a982dc5345f846076e7043c6137
Azzam R Alrashed <azzam.rar@gmail.com>
2023-04-23 04:17:20 +0300

    React Redux TypeScript Quick Start

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,15 @@
 "use client";
 
 import { configureStore } from "@reduxjs/toolkit";
-import { type } from "os";
-import { TypeOfExpression } from "typescript";
 
 export const store = configureStore({
   reducer: {
+    // posts: postsReducer,
+    // comments: commentsReducer,
+    // users: usersReducer,
     //...
   },
 });
 
-export type RootStore = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
